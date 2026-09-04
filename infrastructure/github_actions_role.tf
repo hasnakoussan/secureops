65063945dc561f1a5d8fb016b00757766f120d34
TON_NOM_GITHUB <hasna-deploy@gmail.com>
2026-09-04 13:50:03 +0100

    ci: repartir sur un scanner isolé (Semgrep) + versionner OIDC/rôle GitHub Actions

---
+++ infrastructure/github_actions_role.tf
@@ -0,0 +1,67 @@
+resource "aws_iam_role" "github_actions" {
+  name = "${var.project_name}-github-actions-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:hasnakoussan/secureops:*"
+          }
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name = "${var.project_name}-github-actions-role"
+  }
+}
+
+resource "aws_iam_policy" "github_actions_ecr" {
+  name        = "${var.project_name}-github-actions-ecr-policy"
+  description = "Permissions pour GitHub Actions : push d'images vers ECR"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "ecr:GetAuthorizationToken"
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload"
+        ]
+        Resource = [
+          aws_ecr_repository.auth.arn,
+          aws_ecr_repository.scan.arn,
+          aws_ecr_repository.worker.arn,
+          aws_ecr_repository.dashboard.arn
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_ecr" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = aws_iam_policy.github_actions_ecr.arn
+}
